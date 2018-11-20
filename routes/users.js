509b811751aa882bd798c9ff57add6eb28383cfa
Alice-anjali <alice.anjali1995@gmail.com>
2018-11-20 13:28:21 +0530

    adds new user

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var Admin = require('../models').Admin;
+var User = require('../models').User;
 var app = express();
 
 /* GET users listing. */
@@ -27,6 +28,38 @@ router.post('/login', function(req, res, next) {
   //     }
   //   }
   // })
+});
+
+router.post('/signup', function(req, res, next){
+  if(req.body.pwd1 == req.body.pwd2){
+    User
+    .findOrCreate({where: {email: req.body.email}})
+    .spread((user, created) => {
+      console.log(user.get({
+        plain: true
+      }))
+      console.log(created)
+      if(created){
+        User.update(
+          { username: req.body.username2,
+            password: req.body.pwd1
+          },
+          { where: { email: req.body.email } }
+        )
+        .then((result) => res.status(200).send("Database updated!"))
+        .catch((error) => res.status(400).send(error))
+      }
+      else{
+        res.status(201).send("User already exists!")
+      }
+      })
+  }
+  else{
+    res.status(201).send("Passwords do not match!")
+  }
+
+  // res.redirect('/');
+
 });
 
 module.exports = router;
